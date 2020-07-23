c50b38c03263286ac6385d9e7f2ba5f6e9d69c1d
teruan777 <takuansamurai@yahoo.co.jp>
2020-07-23 15:55:12 +0900

    Active storageの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< Updated upstream
-  root 'rooms#index'
-=======
-  # get 'messages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "rooms#index"
->>>>>>> Stashed changes
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
