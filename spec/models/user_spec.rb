f76d83948051490f194e1ac4b6c6e04180869a98
teruan777 <takuansamurai@yahoo.co.jp>
2020-09-25 21:14:02 +0900

    記述ミス修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,7 +51,7 @@ RSpec.describe User, type: :model do
       @user.save
       another_user = FactoryBot.build(:user, email: @user.email)
       another_user.valid?
-     expect(another_user.errors.full_messages).to include("Email has already been taken")
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
     end
 
   end
